0b1abfee68fb2dd7465c95063f8b387cdd90661d
Krish <krishpatel4020@gmail.com>
2026-07-12 13:18:56 +0530

    modified BubbleSort algorithm

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,23 +1,25 @@
 public class BubbleSort {
     public static void main(String[] args){
         int[] arr = {5,4,3,2,1};
-        bubbleSort(arr, arr.length);
+        bubbleSort(arr, arr.length, 0);
         for(int i : arr){
             System.out.println(i);
         }
     }
-    static void bubbleSort(int arr[], int len){
+    static void bubbleSort(int arr[], int len, int counter){
         if(len == 1){
             return;
         }
-        
-        for(int i=0; i<len-1; i++){
-            if(arr[i] > arr[i+1]){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
+        if(counter == len - 1){
+            bubbleSort(arr, len-1, 0);
+            return;
         }
-        bubbleSort(arr, len-1);
+        if(arr[counter] > arr[counter+1]){
+            int temp = arr[counter];
+            arr[counter] = arr[counter+1];
+            arr[counter+1] = temp;
+        }
+        bubbleSort(arr, len, counter+1);
+        
     }
 }
